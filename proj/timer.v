fbdb4a3a2cdb3eeb3359d1f4f41084902315d179
ammar-faifi <a@ammarf.com>
2020-12-03 12:48:57 +0300

    Change I3 -> I1 and I2 -> I0 and so on

--- proj/timer.v
+++ proj/timer.v
@@ -6,20 +6,20 @@ module timer(input clk, reset, load, CE, [3:0] I1, I0, output LED,  [7:0]seg, [3
 	wire [3:0] D3, D2, D1, D0;
 	wire [26:0] counter;
 	wire CEO, CEO1, CEO2, CEO3;
-	wire zero3, zero2;
+	wire zero1, zero0;
 	wire clk_1hz;
 	assign LED = (CEO1 & CEO2 & CEO3); 
 	
-	assign S2 = (&(~I2))? ( |I3? 4'b1001 : 0) : (I2-1);
-	assign S3 = (&(~I2))? (I3-1) : I3;
+	assign S0 = (&(~I0))? ( |I1? 4'b1001 : 0) : (I0-1);
+	assign S1 = (&(~I0))? (I1-1) : I1;
 	
 	//seconds, with no inputs
 	mod10 s0 (clk, reset, CE, D0, CEO1);
 	mod06 s1 (clk, reset, CEO1, D1, CEO2);
 	
 	//minuites, it can take an inputs 
-	DownCounter m0 (clk, reset, load, CEO2, S2, CEO3, D2);
-	DownCounter m1 (clk, reset, load, CEO3, S3  , CEO, D3);
+	DownCounter m0 (clk, reset, load, CEO2, S0, CEO3, D2);
+	DownCounter m1 (clk, reset, load, CEO3, S1  , CEO, D3);
 
 	DISP7SEG ssd (clk, D0, D1, D2, D3, text_mode, slow, med, fast, error, wrong, seg, an);
 
